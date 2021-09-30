9c4bd839b5b7bcdcdfc017f6a13cd0dd1c54d32a
Votre Nom <Vous@exemple.com>
2021-09-30 12:12:38 +0200

    Updateing scheduler.

--- src/M3/Afficheur.java
+++ src/M3/Afficheur.java
@@ -3,14 +3,16 @@ package M3;
 public class Afficheur implements ObserverDeCapteur{
 
 	int valeur;
+	private Canal canal;
 	
-	public Afficheur() {
-		// TODO Auto-generated constructor stub
+	public Afficheur(Canal canal) {
+		super();
+		this.canal = canal;
 	}
 
 	@Override
 	public void update(Capteur capteur) {
-		// TODO Auto-generated method stub
+		this.canal.update(capteur);
 		this.valeur = capteur.getValue();
 	}
 
