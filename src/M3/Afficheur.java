4f663a7797cbc3e41f3a40f3693dad0a91bde232
Tanguy <tanguy.franchet@precom.fr>
2021-09-30 12:11:11 +0200

    save

--- src/M3/Afficheur.java
+++ src/M3/Afficheur.java
@@ -3,6 +3,8 @@ package M3;
 public class Afficheur implements ObserverDeCapteur{
 
 	int valeur;
+
+	private Canal canal;
 	
 	public Afficheur() {
 		// TODO Auto-generated constructor stub
@@ -10,6 +12,7 @@ public class Afficheur implements ObserverDeCapteur{
 
 	@Override
 	public void update(Capteur capteur) {
+		canal.update(capteur);
 		// TODO Auto-generated method stub
 		this.valeur = capteur.getValue();
 	}
--- src/M3/Canal.java
+++ src/M3/Canal.java
@@ -2,6 +2,7 @@ package M3;
 
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Canal implements ObserverDeCapteurAsync{
 
@@ -13,10 +14,19 @@ public class Canal implements ObserverDeCapteurAsync{
 
     private Update update;
 
-    public Future<Integer> update(Capteur c){
-        Future<Integer> f = null;
-        return f;
+    public Canal(Afficheur affiche, CapteurImpl capteur, ScheduledExecutorService schedule){
+        this.cimpl = capteur;
+        this.affiche = affiche;
+        this.schedule = schedule;
+        this.update = new Update();
     }
+
+    public Future<Integer> update(Capteur c){
+        update = new Update();
+        Long l = 1000L;
+        return schedule.schedule(update, l, TimeUnit.MILLISECONDS);
+        
+        }
 
     public Future<Integer> getValue(){
         Future<Integer> f = null;
