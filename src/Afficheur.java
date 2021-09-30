4550f71412e0d04aaf1e42254b77942e253e0b73
Votre Nom <Vous@exemple.com>
2021-09-30 10:43:22 +0200

    init classes m3 model.

---
+++ src/Afficheur.java
@@ -0,0 +1,16 @@
+
+public class Afficheur implements ObserveDeCapteur{
+
+	int valeurs;
+	
+	public Afficheur() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(Capteur capteur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/ObserveDeCapteur.java
@@ -0,0 +1,5 @@
+
+public interface ObserveDeCapteur {
+
+	void update(Capteur capteur);
+}
